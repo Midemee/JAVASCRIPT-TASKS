41ea55e6f6c1fe68421dc8bf3061eee25ffe2bc8
Midemee <ashiwajuaramide@gmail.com>
2026-05-08 09:33:07 +0100

    Update

--- EASTER EGGS/KOLANUT/discount.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-
-const input = Number(prompt("enter price: "))
-const input1 = Number(prompt("enter discount: "))
-
-const discount = input1 / 100 * input
-const price = input - discount
-
-console.log(price)
--- EASTER EGGS/KOLANUT/pinvalidator.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-
-const input = Number(prompt("enter digit: "))
-
-if(input > 1000 && input < 9999){
-console.log("valid pin")
-} else{
-console.log("invalid pin")
-}
